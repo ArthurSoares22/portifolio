014f8ccdbbfdf17bafd378fa7e541cb17575cf53
ArthurSoares22 <brictone47@hotmail.com>
2024-10-27 13:10:13 -0300

    portifolio

---
+++ src/script.js
@@ -0,0 +1,44 @@
+
+    // Inicializando o ScrollReveal
+    ScrollReveal().reveal('#sobre ', { 
+        origin: 'left', // Animação vinda da esquerda
+        distance: '50px', 
+        duration: 1000, 
+        easing: 'ease-in-out',
+        reset: true,  // Repetir a animação ao rolar novamente
+        interval: 200
+    });
+
+    ScrollReveal().reveal('#projetos', { 
+        origin: 'bottom', // Animação vinda de baixo
+        distance: '50px', 
+        duration: 1000, 
+        interval: 200, // Intervalo entre cada item
+        easing: 'ease-in-out',
+        reset: true  // Repetir a animação ao rolar novamente
+    });
+
+    ScrollReveal().reveal('#home', { 
+        origin: 'bottom', // Animação vinda de baixo
+        distance: '50px', 
+        duration: 1000, 
+        interval: 200, // Intervalo entre cada item
+        easing: 'ease-in-out',
+        reset: true  // Repetir a animação ao rolar novamente
+    });
+    ScrollReveal().reveal('#contatos', { 
+        origin: 'bottom', // Animação vinda de baixo
+        distance: '50px', 
+        duration: 1000, 
+        interval: 200, // Intervalo entre cada item
+        easing: 'ease-in-out',
+        reset: true  // Repetir a animação ao rolar novamente
+    });
+
+    ScrollReveal().reveal('nav', {
+        origin: 'top',
+        distance: '20px',
+        duration: 800,
+        easing: 'ease-in-out'
+    });
+    
